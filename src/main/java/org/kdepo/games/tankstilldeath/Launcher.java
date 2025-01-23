7aad34721a00fda5957464b63c27db252e8c8db1
Andrei <andrei@nitro.notebook>
2025-01-23 09:47:17 +0500

    Added configuration to make executable jar

--- src/main/java/org/kdepo/games/tankstilldeath/Launcher.java
+++ src/main/java/org/kdepo/games/tankstilldeath/Launcher.java
@@ -14,6 +14,7 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
+// "mvn clean compile assembly:single" to get executable jar
 public class Launcher {
 
     public static void main(String[] args) {
